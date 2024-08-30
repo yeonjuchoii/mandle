f3a84b22058f4c7f4f833ea3712938228cce7c2c
yeonjuchoii <yeonjuchoii00@gmail.com>
2024-08-30 15:36:32 -0400

    commit message

--- Mandle/Loading.swift
+++ Mandle/Loading.swift
@@ -4,20 +4,28 @@
 import SwiftUI
 
 struct Loading: View {
+    @State var isMLon = false
+    let input : SkinProblemClassifierInput
+    
     var body: some View {
-        VStack{
-            //how to fit the image to scale
-            Image("LoadingIcon")
-            Text("Loading...")
+        NavigationStack{
+            VStack{
+                //how to fit the image to scale
+                Image("LoadingIcon")
+                Text("Loading...")
+                    .onAppear(let model = try? SkinProblemClassifier())
+                    .onDisappear(NavigationLink(destination: {ResultScreen()}))
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(hex : "8D807D"))
+            
+            //onappear: 화면이 보였을떄 실행 --> parameter image with let, model creation
+            //navigationstack 화면 넘기기 to result screen ??
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(hex : "8D807D"))
-        
-        //onappear: 화면이 보였을떄 실행 --> parameter image with let, model creation
-        //navigationstack 화면 넘기기 to result screen
     }
 }
 
+/*
 #Preview {
     Loading()
-}
+} */
